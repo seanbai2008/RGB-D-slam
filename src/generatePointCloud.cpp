7c0dea8d2b9f49e8e22aa0df1598f827295a8779
Fan Bai <sean@otsaw.com>
2017-07-19 11:11:45 -0700

    create point cloud from sample

--- src/generatePointCloud.cpp
+++ src/generatePointCloud.cpp
@@ -31,24 +31,30 @@ using namespace std;
      cv::Mat rgb, depth;
      // 使用cv::imread()来读取图像
      // API: http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#cv2.imread
-     rgb = cv::imread( "./data/rgb.png" );
+     rgb = cv::imread( "/home/fan/test_dir/develop/src/RGB-D/src/data/rgb.png" );
      // rgb 图像是8UC3的彩色图像
      // depth 是16UC1的单通道图像，注意flags设置-1,表示读取原始数据不做任何修改
-     depth = cv::imread( "./data/depth.png", -1 );
+     depth = cv::imread( "/home/fan/test_dir/develop/src/RGB-D/src/data/depth.png", -1 );
 
      // 点云变量
      // 使用智能指针，创建一个空点云。这种指针用完会自动释放。
      PointCloud::Ptr cloud ( new PointCloud );
      // 遍历深度图
-     for (int m = 0; m < depth.rows; m++)
+
+
+     for (int m = 0; m < depth.rows; m++){
          for (int n=0; n < depth.cols; n++)
          {
+
              // 获取深度图中(m,n)处的值
              ushort d = depth.ptr<ushort>(m)[n];
+
+
              // d 可能没有值，若如此，跳过此点
              if (d == 0)
                  continue;
              // d 存在值，则向点云增加一个点
+
              PointT p;
 
              // 计算这个点的空间坐标
@@ -61,16 +67,19 @@ using namespace std;
              p.b = rgb.ptr<uchar>(m)[n*3];
              p.g = rgb.ptr<uchar>(m)[n*3+1];
              p.r = rgb.ptr<uchar>(m)[n*3+2];
-
              // 把p加入到点云中
              cloud->points.push_back( p );
          }
+       }
+
      // 设置并保存点云
      cloud->height = 1;
      cloud->width = cloud->points.size();
      cout<<"point cloud size = "<<cloud->points.size()<<endl;
+
+     //True if no points are invalid
      cloud->is_dense = false;
-     pcl::io::savePCDFile( "./pointcloud.pcd", *cloud );
+     pcl::io::savePCDFile( "/home/fan/test_dir/develop/src/RGB-D/pointcloud.pcd", *cloud );
      // 清除数据并退出
      cloud->points.clear();
      cout<<"Point cloud saved."<<endl;
